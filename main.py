c167af3489d26dcbd06006c43d4e50a465729fe9
Qunos <male.korps@gmail.com>
2025-07-31 14:43:46 +0300

    2. Some updates

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import Player
 
 def main():
-    pygame.init
+    pygame.init()
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        def fill(self, color):
-            self.color = "black"
+
+        screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
+        
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+
 if __name__ == "__main__":
     main()
